bae054cf441fd4d90a2d26d4e4cb8efdf2d0f533
han nguyen <han.nguyen@outlook.com>
2019-10-24 15:35:17 -0500

    add 0_csharp and gitignore

---
+++ 0_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿// using System;
+using sc = System.Console;  // import with alias
+
+
+namespace HelloWorld.Client
+{
+
+  /// <summary>
+  /// contains the start of the program
+  /// </summary>
+  internal class Program
+  {
+    /// <summary>
+    /// starts the program
+    /// </summary>
+    /// <param name="args"></param>
+
+
+    private static void Main(string[] args)
+    {
+      sc.WriteLine("Hello World!");
+    }
+  }
+}
